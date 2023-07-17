41a45423afdd8df20764c11175ee5cc70812dd70
eben <ebencito88@gmail.com>
2023-07-17 19:56:05 +0100

    Tablero

---
+++ index.js
@@ -0,0 +1,30 @@
+function Player(){
+    this.sprite=document.getElementById("player")
+    this.topPosition=900
+    this.leftPosition=900
+    this.movimiento=function(direccion){
+        if(direccion="ArrowUp"){
+            this.topPosition-=150
+            this.sprite.style.top=this.topPosition+"px"
+        }
+        if(direccion="ArrowDown"){
+            this.topPosition+=150
+            this.sprite.style.top=this.topPosition+"px"
+        }
+        if(direccion="ArrowRight"){
+            this.leftPosition+=150
+            this.sprite.style.left=this.leftPosition+"px"
+        }
+        if(direccion="ArrowLeft"){
+            this.leftPosition-=150
+            this.sprite.style.left=this.leftPosition+"px"
+        }
+
+    }
+}
+
+const player=new Player()
+
+const direccion=window.addEventListener("keydown",function(e){
+    player.movimiento(e.key)
+})
