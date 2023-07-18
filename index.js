d2b1f9ee2017eb654fa3f4889af4f7ef4c218831
fayna <faynamartinotamendi@MacBook-Pro-de-Fayna.local>
2023-07-18 20:07:58 +0100

    principal

--- index.js
+++ index.js
@@ -54,63 +54,31 @@ let parejas=["pareja1","pareja1","pareja2","pareja2","pareja3","pareja3","pareja
 let parejasAleatorias= parejas.sort(function(a,b){
     return Math.random()-0.5
   })
+  
+  let celdas = [];
 
-function cartasprueba(arriba,izquierda){
-if(arriba===0 && izquierda===0){
-    celda1.classList.remove("trasera")
-    celda1.classList.add(parejasAleatorias[0])
-}if(arriba===0 && izquierda===150){
-    celda2.classList.remove("trasera")
-    celda2.classList.add(parejasAleatorias[1])
-    }if(arriba===0 && izquierda===300){
-        celda3.classList.remove("trasera")
-        celda3.classList.add(parejasAleatorias[2])
-        }if(arriba===0 && izquierda===450){
-            celda4.classList.remove("trasera")
-            celda4.classList.add(parejasAleatorias[3])
-            }if(arriba===150 && izquierda===0){
-                celda5.classList.remove("trasera")
-                celda5.classList.add(parejasAleatorias[4])
-                }if(arriba===150 && izquierda===150){
-                    celda6.classList.remove("trasera")
-                    celda6.classList.add(parejasAleatorias[5])
-                    }if(arriba===150 && izquierda===300){
-                        celda7.classList.remove("trasera")
-                        celda7.classList.add(parejasAleatorias[6])
-                        }if(arriba===150 && izquierda===450){
-                            celda8.classList.remove("trasera")
-                            celda8.classList.add(parejasAleatorias[7])
-                            }if(arriba===300 && izquierda===0){
-                                celda9.classList.remove("trasera")
-                                celda9.classList.add(parejasAleatorias[8])
-                                }if(arriba===300 && izquierda===150){
-                                    celda10.classList.remove("trasera")
-                                    celda10.classList.add(parejasAleatorias[9])
-                                    }if(arriba===300 && izquierda===300){
-                                        celda11.classList.remove("trasera")
-                                        celda11.classList.add(parejasAleatorias[10])
-                                        }if(arriba===300 && izquierda===450){
-                                            celda12.classList.remove("trasera")
-                                            celda12.classList.add(parejasAleatorias[11])
-                                            }if(arriba===450 && izquierda===0){
-                                                celda13.classList.remove("trasera")
-                                                celda13.classList.add(parejasAleatorias[12])
-                                                }if(arriba===450 && izquierda===150){
-                                                    celda14.classList.remove("trasera")
-                                                    celda14.classList.add(parejasAleatorias[13])
-                                                    }if(arriba===450 && izquierda===300){
-                                                        celda15.classList.remove("trasera")
-                                                        celda15.classList.add(parejasAleatorias[14])
-                                                        }if(arriba===450 && izquierda===450){
-                                                            celda16.classList.remove("trasera")
-                                                            celda16.classList.add(parejasAleatorias[15])
-                                                            }
-}
+  for (let i = 1; i <= 16; i++) {
+    let celda = document.getElementById("c" + i);
+    celdas.push(celda);
+    
+  }
+  
+  function cartasprueba(arriba, izquierda) {
+    let posicion = (arriba / 150) * 4 + (izquierda / 150);
+    let celda = celdas[posicion];
+  
+    celda.classList.remove("trasera");
+    celda.classList.add(parejasAleatorias[posicion]);
+  }
+
+  
+
 
 const player=new Player()   
 
 const direccion=window.addEventListener("keydown",function(e){
     player.movimiento(e.key)
 })
+
 
 
